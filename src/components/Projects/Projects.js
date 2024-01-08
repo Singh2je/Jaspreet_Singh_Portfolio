69b3f52afb0a0cbab648b47481f145b6f1bdb973
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2024-01-08 00:59:34 -0500

    Project Ditto Added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import ProjectImage from "../../Assets/Projects/ProjectImage.png";
 import FinanceTracker from "../../Assets/Projects/FinanceTrackerStats.png";
 import CalendarTech from "../../Assets/Projects/CalendarTech.png";
+import Ditto from "../../Assets/Projects/Ditto.png";
 
 function Projects() {
   return (
@@ -44,20 +45,20 @@ function Projects() {
             <ProjectCard
               imgPath={ProjectImage}
               isBlog={false}
-              title="Ditto - Audio Transcription Software"
-              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen boo"
-              ghLink=""
-              demoLink=""              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={ProjectImage}
-              isBlog={false}
               title="Project 4"
               description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen boo"
               ghLink=""
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card bright-image">
+            <ProjectCard
+              imgPath={Ditto}
+              isBlog={false}
+              title="Ditto - Audio Transcription Software"
+              description="Ditto is a revolutionary AI-powered product designed to replace manual meeting note-taking. It records and transcribes conversations, categorizes information, and generates concise summaries. With Ditto, users can capture every detail of one-on-one meetings, group sessions, or conference calls, ensuring a complete and accurate record of the conversation. The software's ability to create easily digestible summaries allows users to save time and focus on essential tasks, while still having the convenience of referring back to meeting notes whenever needed."
+              ghLink="https://github.com/Singh2je/Ditto"
               demoLink=""
             />
           </Col>
